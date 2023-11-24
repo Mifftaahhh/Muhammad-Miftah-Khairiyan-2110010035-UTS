6e2a950c8894f012abe4d58f682900084ddb97cd
Muhammad Miftah Khairiyan <58870271+Mifftaahhh@users.noreply.github.com>
2023-11-25 01:57:51 +0800

    Add files via upload

---
+++ UTS/src/main/java/com/mycompany/uts/UTS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uts;
+
+/**
+ *
+ * @author mkhai
+ */
+public class UTS {
+
+    public static void main(String[] args) {
+        System.out.println("Muhammad Miftah Khairiyan");
+        System.out.println("5B Reg Pagi BJB");
+        System.out.println("2110010035");
+        System.out.println("UTS Pemrograman Berbasis Objek");
+    }
+}
